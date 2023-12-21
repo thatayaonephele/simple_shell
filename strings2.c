add153163d8729b1fe2c8e770791cff34903bc91
thatayaonephele <thatayaonephele@gmail.com>
2023-12-20 22:29:30 -0800

    Determine the string length of inputed char data type

---
+++ strings2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * str_len - A function that evaluates the string length of inputed char data
+ * @chr_str: The input string parameter to be evaluated
+ * Return: The input string parameter length
+ */
+int str_len(char *chr_str)
+{
+	int length; /*Iterate thru str until the null terminator is reached*/
+
+	for (length = 0; *chr_str != 0; length++)
+		chr_str++; /*increment counter var to count chrs*/
+	return (length); /*The string length (if any)*/
+}
