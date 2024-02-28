6b0a64b07a3c2fb101107e446e9b185dca2c5e24
thatayonephele <thatayaonephele@gmail.com>
2024-02-28 08:57:22 -0800

    Delimeter check function

--- interactive.c
+++ interactive.c
@@ -1,4 +1,3 @@
-
 #include "shell.h"
 
 /**
@@ -14,4 +13,27 @@ int int_sh(data_t *d)
 		return (1); /*yes, it means we in interactive mode*/
 	else
 		return (0); /*No it's not thus we not in interactive mode*/
+}
+/**
+ * delim_check - Verfies if delimeter(s) are present in a set of characters
+ * @ch: The character input to be verified
+ * @delimeter: The string delimeter parameter
+ * Return: 1 if delimeters are present, else 0
+ */
+
+int delim_check(char ch, char *delimeter)
+{
+	if (!(*delimeter))
+	{
+		return (0);
+	}
+	else
+	{
+		while (!(*delimeter) == false)
+		{
+			if (*delimeter++ == ch)
+				return (1);
+		}
+	}
+	return (0);
 }
