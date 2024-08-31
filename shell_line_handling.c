1976893c8e99bed1712dece7829ff95eac10f4fc
thatayaonephele <thatayaonephele@gmail.com>
2024-08-31 09:29:12 -0700

    Update of Signal Handler

--- shell_line_handling.c
+++ shell_line_handling.c
@@ -8,16 +8,28 @@
  *
  * Return: Data read from the buffer
  */
-
 ssize_t buff_read(data_t *d, char *my_buffer, size_t *object_size)
 {
-	ssize_t bytes_read;
+    ssize_t bytes_read = 0;
 
-	bytes_read = 0;
+    if (object_size != NULL)
+        return (0);
 
-	if (d != NULL && my_buffer != NULL && object_size != NULL)
-	{
-		bytes_read = write(STDOUT_FILENO, my_buffer, *object_size);
-	}
-	return (bytes_read);
+    bytes_read = read((*d).readfd, my_buffer, READ_BUF_SIZE);
+
+    if (bytes_read >= 0)
+        *object_size = bytes_read;
+        
+    return (bytes_read);
+}
+/**
+ * block_ctrl_c - A function that blocks the ctrl-C shortcut function
+ * @sig_num: An unused signal number
+ * Return: Nothing (void function)
+ */
+void block_ctrl_c(__attribute__((unused))int sig_num)
+{
+        my_puts("\n");
+        my_puts("$ ");
+        _putchar(BUF_FLUSH);
 }
