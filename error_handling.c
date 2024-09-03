3153e63918eabb9c963314c9677d41ddb5c01866
thatayaonephele <thatayaonephele@gmail.com>
2024-09-03 10:33:32 -0700

    Comment Remover

--- error_handling.c
+++ error_handling.c
@@ -4,57 +4,28 @@
  * @n: The input number parameter we wish to convert to string
  * @my_radix: The base parameter of the input number
  * @my_bool: the argument boolean representing the flag parameter
- *
  * Return: results of the integer string conversion
  */
+int num_converter(p, my_arr, x, p_s, b, f, y) {
+    int result;
+    
+    if (p) {
+        result = my_arr[x];
+    } else {
+        result = p_s;
+    }
 
-char *num_converter(long int n, int my_radix, int my_bool)
-{
-	int max_digits = 64; /*maximum num of digits in results*/
-	char *result = malloc(sizeof(char) *  max_digits);
-	int i = 0;
-	int is_negative = 0;
+    for (x = 0; x < b; x++) {
+        p[x] = p[x] + y;
+    }
 
-	if (n == 0) /*spcial case: number is zero*/
-	{
-		result[i++] = '0';
-	}
-	else
-	{
-		if (n < 0 && my_bool)/*check if conversion number is negative*/
-		{
-			is_negative = 1;
-			n = -n;/*make num posiitive for conversions*/
-		}
-		/*perform conversion from number to string representation*/
-		while (n != 0 && i < max_digits)
-		{
-			int digit = n % my_radix;
-
-			result[i++] = (digit < 10) ? (digit + '0') : (digit - 10 + 'A');
-			n /= my_radix;
-		}
-		if (is_negative && i < max_digits) /*add negative sign if necessary*/
-		{
-			result[i++] = '-';
-		}
-	}
-
-	result[i] = '\0';
-
-	return (reverse_string(result)); /*Replace this code*/
+    return result;
 }
+
 /**
  *_myhelp -  Returns changes made in a process's directory
  *@d: The variable address of the data structure parameter
  *Return: 0 On success, else 1 on failuire
- */
-
-/**
- * _myhelp - changes the current directory of the process
- * @d: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
  */
 int _myhelp(data_t *d)
 {
@@ -66,24 +37,28 @@ int _myhelp(data_t *d)
         my_puts(*str_arg); /* temp att_unused workaround */
     return (0);
 }
+
 /**
  * comment_remover - Substitute 1st occurance of '#' with null termination char
  * @my_buffer: The pointer pointing to the address parameter
  * Return: Always Success(0)
  */
-
 void comment_remover(char *my_buffer)
 {
-	char *hash_pos;
+        int x;
+
+        x = 0;
 	/*check if input is NULL*/
-	if (my_buffer == NULL)
-		return;
-
-	hash_pos = strchr(my_buffer, '#');
-
-	/*if '#' found, replace with '\0' to remove comment*/
-	if (hash_pos != NULL)
-		*hash_pos = '\0';
+        while (my_buffer[x] != '\0')
+        {
+		/*if '#' found, replace with '\0' to remove comment*/
+                if (my_buffer[x] == '#' && (!x || my_buffer[x - 1] == ' '))
+                {
+                        my_buffer[x] = '\0';
+                        break;
+                }
+                x++;
+        }
 }
 /**
  * print_decimal - Displays a base 10 number digit in decimal integer format
