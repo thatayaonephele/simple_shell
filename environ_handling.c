e27debe0582ff50317548717767ce6a1ae63b358
thatayaonephele <thatayaonephele@gmail.com>
2024-09-09 06:52:35 -0700

    Hot Fix

--- environ_handling.c
+++ environ_handling.c
@@ -6,37 +6,23 @@
  *
  * Return: 0 on success removal, 1 on failure
  */
-
 int unset_my_env(data_t *d)
 {
-	int i, j;
-	char **new_env;
+    int x;
 
-	if (d->arg == NULL)
-		return (1);
+    if ((*d).argc == 1)
+    {
+        error_puts("Too few arguments.\n");
+        return (1);
+    }
+    x = 1;
+    while (x <= (*d).argc)
+    {
+        _unsetenv(d, (*d).argv[x]);
+        x++;
+    }
 
-	for (i = 0; d->environ[i] != NULL; i++)
-		;
-	new_env = malloc(sizeof(char *) * i);
-
-	if (new_env == NULL)
-		return (1);
-
-	for (i = 0, j = 0; d->environ[i] != NULL; i++)
-	{
-		if (lexi_cmp(d->environ[i], d->arg) != 0)
-		{
-			new_env[j] = d->environ[i];
-			j++;
-		}
-	}
-	new_env[j] = NULL;
-
-	free(d->environ);
-
-	d->environ = new_env;
-
-	return (0);
+    return (0);
 }
 /**
  * append_env_list - Appends vars to a linked list of env
