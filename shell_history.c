66a2cf4d75ba51391fa410e3414894f577955716
thatayaonephele <thatayaonephele@gmail.com>
2024-09-10 19:23:18 -0700

    Index History Function

--- shell_history.c
+++ shell_history.c
@@ -4,3 +4,15 @@
  *index_his - Reorder index numbers after ammendements of the history list
  *@d: Structure containing potential arguments. Used to maintain
  */
+int index_his(data_t *d)
+{
+    stringnode_t *my_node = (*d).node_his;
+    int x = 0;
+
+    for (; my_node; my_node = (*my_node).next)
+    {
+        (*my_node).num = x++;
+    }
+
+    return ((*d).h_counter = x);
+}
