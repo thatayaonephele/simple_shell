741a157b558cc504206c5c569eb31654cb2b7d0c
thatayaonephele <thatayaonephele@gmail.com>
2024-09-14 03:31:51 -0700

    Atoit Error Function

--- var_handling.c
+++ var_handling.c
@@ -25,3 +25,34 @@ int curr_char_buff(data_t *data, char *ch_buff, size_t *ptr)
 	}
 	return (0);
 }
+/**
+ * atoi_err - converts a string to an integer
+ * @sub_str: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ *       -1 on error
+ */
+int atoi_err(char *sub_str)
+{
+	unsigned long int output = 0;
+        int x = 0;
+
+        if (*sub_str == '+')
+                sub_str++;
+
+        x = 0;
+        while (sub_str[x] != '\0')
+        {
+                if (*(sub_str + x) <= '9' && *(sub_str + x) >= '0')
+                {
+                        output = output * 10;
+                        output = output + (sub_str[x] - '0');
+                        if (output > INT_MAX)
+                                return (-1);
+                }
+                else
+                        return (-1);
+
+                x = x + 1;
+        }
+        return (output);
+}
