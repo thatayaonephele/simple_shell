726f11d10642b67f89b5b9c74da36465296ed696
thatayaonephele <thatayaonephele@gmail.com>
2024-09-15 09:46:01 -0700

    A function that prints the str arr of a linked list struct

--- file_handling.c
+++ file_handling.c
@@ -50,3 +50,28 @@ char *char_dup(char *str_path, int src, int dest)
     *(my_buff + z) = '\0';
     return (my_buff);
 }
+/**
+ * display_str_element - Displays the string element of a stringnode_t linked list
+ * @my_h: A pointer to the first node in the list
+ *
+ * Return: The total number of nodes in the list (size of list)
+ */
+size_t display_str_element(const stringnode_t *my_h)
+{
+    size_t size_of_list = 0;
+
+    for (; my_h; my_h = (*my_h).next, size_of_list++)
+    {
+        if ((*my_h).str)
+        {
+            my_puts((*my_h).str);
+        }
+        else
+        {
+            my_puts("(nil)");
+        }
+        my_puts("\n");
+    }
+
+    return (size_of_list);
+}
