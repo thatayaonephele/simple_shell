47557d10ca4e9fe09db3fc393585b6b1bcc191ed
thatayaonephele <thatayaonephele@gmail.com>
2024-09-15 09:20:47 -0700

    A function that frees the data struct fields

--- data.c
+++ data.c
@@ -1,39 +1,5 @@
 #include "shell.h"
 /**
- * free_data - A function that frees the data_t structure fields
- * @x: A function that acts as a boolean that frees all fields of the structure
- * @data: The variable address of the data structure
- *
- * Return: void
- */
-
-void free_data(data_t *d, int x)
-{
-	stringnode_t *current_his, *next_his;
-
-	if (!data)
-		return;
-
-	if (x)
-	{
-		free(data->arg);
-		free(data->argv);
-		free(data->cmd_path);
-		free(data->file_name);
-		free(data->_env);
-	}
-	current_his = data->node_his;
-
-	while (current_his)
-	{
-		next_his = current_his->next;
-		free(current_his->s);
-		free(current_his);
-		current_his = next_his;
-	}
-	free(data);
-}
-/**
  * clear_d - A function that initializes the data_t structure
  * @d: The struct parameter addr location
  */
--- list_handling_helper.c
+++ list_handling_helper.c
@@ -16,3 +16,33 @@ void free_str_array(char **str_array)
 
     free(temp);
 }
+/**
+ * free_data - A function that frees the data_t structure fields
+ * @x: A function that acts as a boolean that frees all fields of the structure
+ * @data: The variable address of the data structure
+ *
+ * Return: void
+ */
+void free_data(data_t *d, int x)
+{
+    str_free((*d).argv);
+    (*d).argv = NULL;
+    (*d).cmd_path = NULL;
+    if (x)
+    {
+        if (!(*d).cmd_buf)
+            free((*d).arg);
+        if ((*d)._env)
+            free_mem(&(*d)._env);
+        if ((*d).my_his)
+            free_mem(&(*d).my_his);
+        if ((*d).alias)
+            free_mem(&(*d).alias);
+        str_free((*d).environ);
+        (*d).environ = NULL;
+        free_vars((void **)(*d).cmd_buf);
+        if ((*d).r_fd > 2)
+            close((*d).r_fd);
+        _putchar(BUF_FLUSH);
+    }
+}
